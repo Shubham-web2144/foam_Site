26d3fe7f715d53aa3c71c14addbaed41a2f6af87
Shubham-web2144 <shubhamkjadhav22@gmail.com>
2021-12-25 21:01:03 +0530

    sec

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const navIcon = document.querySelector('.nav_icon');
 const nav = document.querySelector('.nav');
 const navIconx = document.querySelector('.nav_icon-x '); 
+const logo = document.querySelector('.logo');
 
 navIcon.addEventListener('click', () => {
     nav.classList.add('active');
@@ -8,4 +9,8 @@ navIcon.addEventListener('click', () => {
 
 navIconx.addEventListener('click', () => {
     nav.classList.remove('active');
+});
+
+logo.addEventListener("click", ()=>{
+    window.location.href = "../index.html";
 });
